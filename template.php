6865c472969ccb0c6ee5f10612bce5bda1ac5e9c
indigoxela <xela@indigofloat.at>
2026-06-10 12:58:50 +0200

    Issue #4: Fix different variable in preprocessor since core 1.34 (#5)

--- template.php
+++ template.php
@@ -64,9 +64,11 @@ function bedrock_preprocess_page(&$variables) {
  * Implements hook_preprocess_HOOK().
  */
 function bedrock_preprocess_layout(&$variables) {
-  if (isset($variables['layout_info']['flexible'])) {
+  // As of core 1.34.0 "layout_info" is null, will get removed in core 2.x.
+  $info = $variables['layout_info'] ?? $variables['layout_template_info'];
+  if ($info && !empty($info['flexible'])) {
     // Add CSS class to layout based on flexible template machine name.
-    $variables['classes'][] = backdrop_clean_css_identifier('layout-' . $variables['layout_info']['name']);
+    $variables['classes'][] = backdrop_clean_css_identifier('layout-' . $info['name']);
   }
 }
 
